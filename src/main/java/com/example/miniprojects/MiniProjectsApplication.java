838d0f6233d434e5c00ddf1a3de9b305c7e9c18d
micah810 <micahlarson810@gmail.com>
2023-05-08 13:27:34 -0500

    first commit

---
+++ src/main/java/com/example/miniprojects/MiniProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniprojects/MiniProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
